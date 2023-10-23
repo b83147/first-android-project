e81810245489ed574492c0b298021c587a7d76a8
b83147 <b83147@yandex.ru>
2023-10-23 15:27:56 +0500

    Новый проект на андроид

---
+++ app/src/main/java/ru/asp/p0031_myapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.asp.p0031_myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
